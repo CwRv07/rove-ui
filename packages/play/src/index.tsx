cc6dad5c2dc503be8a3088f9e0d165d4cb2f29a9
CwRv07 <Rv_Jiang@outlook.com>
2023-05-17 14:16:49 +0800

    test(play): 测试按需引入能力

--- packages/play/src/index.tsx
+++ packages/play/src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'rove-ui/style'
-import { Button } from 'rove-ui'
+import 'rove-ui/style.css'
+import Button from 'rove-ui/dist/button'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
-  <Button color="red">红色按钮</Button>
+  <>
+    <Button color="red">红色按钮</Button>
+  </>
 )
